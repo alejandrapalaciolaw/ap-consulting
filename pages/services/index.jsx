4e3d7a856067b9988392eacd3f3bbfe759257821
Juan Rios <mrdaniel19996@gmail.com>
2021-04-18 09:10:14 -0500

    services done

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -1,12 +1,21 @@
+import { Button } from "@material-ui/core";
 import React from "react";
+import ActiveLink from "../../components/ActiveLink.component";
 import BoxLayout from "../../components/Content.layout";
 import ServicesListComponent from "../../components/Services/ServicesList.component";
+
+import style from "../../styles/Services.module.css";
 
 const ServicesPage = () => {
   return (
     <div>
       <BoxLayout title="Services">
         <ServicesListComponent />
+        <ActiveLink href="/">
+          <Button variant="text" className={style.back_button}>
+            Back Home
+          </Button>
+        </ActiveLink>
       </BoxLayout>
     </div>
   );
