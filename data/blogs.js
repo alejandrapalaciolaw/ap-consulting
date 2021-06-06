57e19ec8b2624a1eaee0869e3ca74979c0b311db
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-06 18:35:43 -0500

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -1,11 +1,11 @@
 const blogs = [
   {
-    id: "derecho-de-familia",
-    title: "A",
+    id: "blog_001",
+    title: "DERECHO DE FAMILIA",
     description:
       "es la parte del derecho civil que tiene por objeto las relaciones juridicas familiares, relaciones conyugales,paterno-filiales, tanto en su aspecto personal como patrimoniales ",
     image:
-      "https://images.unsplash.com/photo-1471107340929-a87cd0f5b5f3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1866&q=80",
+      "https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   },
   {
     id: "blog_002",
