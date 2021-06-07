19204f6ddefa8b8b72303193ebdc3e508d815c42
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-07 06:13:53 -0500

    Update services.js

--- data/services.js
+++ data/services.js
@@ -16,7 +16,7 @@ const services = [
     id: "learn-more03",
     title: "Traductor Independiente (freelance)",
     description:
-      "",
+      "traduccion de textos",
     image:
       "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
   },
