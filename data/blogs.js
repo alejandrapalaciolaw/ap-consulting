c7c6255043058c8799d8cf351baa3a08b3ee171a
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-06 19:16:06 -0500

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -1,7 +1,7 @@
 const blogs = [
   {
     id: "blog_001",
-    title: "DERECHO DE FAMILIA",
+    title: "Derecho de Familia",
     description:
       "es la parte del derecho civil que tiene por objeto las relaciones juridicas familiares, relaciones conyugales,paterno-filiales, tanto en su aspecto personal como patrimoniales ",
     image:
@@ -9,26 +9,27 @@ const blogs = [
   },
   {
     id: "blog_002",
-    title: "blog 2",
-    description: "Description Description Description Description ",
+    title: "Derecho Penal",
+    description: "El derecho penal comprende la creación y el estudio de las leyes penales, aquellas que contemplan justamente lo que es y lo que no es un delito, así como el acompañamiento y orientación de las decisiones judiciales en la materia. Pero no sólo eso, sino que también de los mecanismos con los que la sociedad se protege a sí misma y la filosofía que existe detrás del castigo y/o la reclusión.",
     image:
-      "https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1560575193-c2c9e886aefe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
   },
   {
     id: "blog_003",
-    title: "blogs 3",
+    title: "Derecho Laboral",
     description:
-      "Description Description Description Description Description Description ",
+      "El Derecho del Trabajo es una rama del Derecho en general, y del Derecho Privado en particular, que regula jurídicamente las relaciones entre empleados y empleadores, en forma individual y colectiva, estableciendo sus derechos y deberes recíprocos. ",
     image:
-      "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
   },
   {
-    id: "compus-baratas",
-    title: "blogs 3",
+    id: "blog_004",
+    title: "Legislacion De Menores",
     description:
-      "Description Description Description Description Description Description ",
+      "Ley 1098 de 2006 Codigo de la infancia y la adolencia, tiene por objeto garantizar a los niños, niñas y adolecentes que se encuentren enel territorio Nacional el ejercicio y disfrute pleno y efectivo de sus derechos fundamentales"
+    ",
     image:
-      "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      "https://images.unsplash.com/photo-1594608661623-aa0bd3a69d98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1159&q=80",
   },
 ];
 
