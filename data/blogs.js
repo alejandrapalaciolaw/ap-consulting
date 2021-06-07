7c22d9954288e2c1741bc9b391b90ad66a71bf24
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-06 19:46:00 -0500

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -3,14 +3,14 @@ const blogs = [
     id: "blog_001",
     title: "Derecho de Familia",
     description:
-      "es la parte del derecho civil que tiene por objeto las relaciones juridicas familiares relaciones conyugales paterno filiales tanto en su aspecto personal como patrimoniales",
+      "",
     image:
       "https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   },
   {
     id: "blog_002",
     title: "Derecho Penal",
-    description: "El derecho penal comprende la creación y el estudio de las leyes penales aquellas que contemplan justamente lo que es y lo que no es un delito, así como el acompañamiento y orientación de las decisiones judiciales en la materia",
+    description: "",
     image:
       "https://images.unsplash.com/photo-1560575193-c2c9e886aefe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
   },
@@ -18,7 +18,7 @@ const blogs = [
     id: "blog_003",
     title: "Derecho Laboral",
     description:
-      "El Derecho del Trabajo es una rama del Derecho en general y del Derecho Privado en particular que regula jurídicamente las relaciones entre empleados y empleadores, en forma individual y colectiva estableciendo sus derechos y deberes recíprocos",
+      "",
     image:
       "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
   },
@@ -26,7 +26,7 @@ const blogs = [
     id: "blog_004",
     title: "Legislacion De Menores",
     description:
-      "Ley 1098 de 2006 Codigo de la infancia y la adolencia tiene por objeto garantizar a los niños niñas y adolecentes que se encuentren en el territorio Nacional el ejercicio y disfrute pleno y efectivo de sus derechos fundamentales"
+      ""
     ",
     image:
       "https://images.unsplash.com/photo-1594608661623-aa0bd3a69d98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1159&q=80",
