4779dc50ddf9695a174540c3ab9a7c6f195c167d
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-12 18:58:10 -0500

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -18,7 +18,7 @@ const blogs = [
     id: "blog_003",
     title: "Derecho Laboral",
     description:
-      "",
+      "es el derecho protector de la clase trabajadora, busca el equilibrio de los factores de producción, el capital de trabajo, garantizar las fuentes de empleo y  productividad, brindan al trabajador la oportunidad de vivir y ofrecerle a su familia una vida plena.",
     image:
       "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
   },
