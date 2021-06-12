8a4bb76d37032886247eeff263611bf3ec63ca0b
alejandrapalaciolaw <81726581+alejandrapalaciolaw@users.noreply.github.com>
2021-06-12 16:25:19 -0500

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -3,7 +3,7 @@ const blogs = [
     id: "blog_001",
     title: "Derecho de familia",
     description:
-      "",
+      "El Derecho de Familia o también llamado Derecho familiar es el conjunto de normas e instituciones jurídicas que regulan las relaciones personales y patrimoniales de los miembros que integran la familia",
     image:
       "https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   },
